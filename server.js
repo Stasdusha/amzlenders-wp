d5919068fbcb55f70f8bed02ba6999eab1199bde
Stas Dushkin <stasdusha@gmail.com>
2018-02-09 17:54:03 +0200

    setup herocu config. Fix bugs

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-import { exists } from 'fs';
-
 var path = require('path');
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, './app')));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Server listening on port' + port);
 });
