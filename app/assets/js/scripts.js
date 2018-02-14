3e6518eb035e71f0610e0c39a8a9dc2b05deb8fd
Stas Dushkin <stasdusha@gmail.com>
2018-02-14 10:34:22 +0200

    Mobile search

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,18 +11,32 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 
-  const searchHeader = $('#search-header');
-  const heroHeight = $('.hero').outerHeight();
+  const MAX_MOBILE_WIDTH = 768;
+  const OFFSET = 6
+
+  const inputOffset = $('#search-hero').offset().top - OFFSET;
+  const isMobil = $(window).width() <= MAX_MOBILE_WIDTH;
+  const searchHeader = isMobil ? $('#search-button') : $('#search-header');
+  const mobileSearchBlock = $('.mobile-search')
+
 
   $(document).scroll(function(event){
-
-    if($(this).scrollTop() > heroHeight) {
+    if($(this).scrollTop() > inputOffset) {
       searchHeader.show()
     } else {
       searchHeader.hide()
     }
-
   })
 
+  $('#search-button').click(function(){
+    mobileSearchBlock.show().css('display', 'flex');
+    $('.mobile-search .search__input').focus()
+  })
 
+  $('.search__close').click(hideMobileSearch)
+  mobileSearchBlock.focusout(hideMobileSearch)
 });
+
+function hideMobileSearch(){
+  mobileSearchBlock.hide()
+}
